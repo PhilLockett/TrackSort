f9b0d55b849a39b533d6b7745c9e3e920cb164e6
Phil Lockett <phillockett65@gmail.com>
2021-05-17 16:29:55 -0400

    Minor code clean up.

--- Shuffle.cpp
+++ Shuffle.cpp
@@ -35,115 +35,19 @@
 #include "Utilities.h"
 #include "Configuration.h"
 
-#if 0
-static std::vector<Side> addTracksToSides(const std::vector<Track> & tracks, size_t duration, size_t count)
-{
-    // std::cout << "Add tracks to sides\n";
-    std::vector<Side> sides;
-    sides.reserve(count);
-    Side side{};
-    for (int i = 0; i < count; ++i)
-    {
-        const std::string title{"Side " + std::to_string(i+1)};
-        side.setTitle(title);
-        sides.push_back(side);
-    }
 
-    bool forward{true};
-    int direction{1};
-    std::vector<Side>::iterator forth{sides.begin()};
-    std::vector<Side>::reverse_iterator back{sides.rbegin()};
-    std::vector<Track>::const_iterator track{tracks.begin()};
-    for (int attempts = 0; attempts < count; ++attempts)
-    {
-        if (forward)
-        {
-            (*forth).push(*track);
-            if ((*forth).getDuration() > duration)
-            {
-                (*forth).pop();
-                continue;
-            }
-
-            std::advance(track, 1);
-            if (track == tracks.end())
-                break;
-
-            std::advance(forth, 1);
-            attempts = 0;
-
-            if (forth == sides.end())
-            {
-                forward = false;
-                forth = sides.begin();
-            }
-        }
-        else
-        {
-            (*back).push(*track);
-            if ((*back).getDuration() > duration)
-            {
-                (*back).pop();
-                continue;
-            }
-
-            std::advance(track, 1);
-            if (track == tracks.end())
-                break;
-
-            std::advance(back, 1);
-            attempts = 0;
-
-            if (back == sides.rend())
-            {
-                forward = true;
-                back = sides.rbegin();
-            }
-        }
-    }
-    // if (side.size() != 0)
-    // {
-    //     const auto count{sides.size() + 1};
-    //     const std::string title{"Side " + std::to_string(count)};
-    //     side.setTitle(title);
-    //     sides.push_back(side);
-    //     side.clear();
-    // }
-
-    return sides;
-}
-
-static bool leftCondition(size_t required, size_t current)
-{
-    if (required < current)
-        return true;
-
-    return false;
-}
-
-static bool rightCondition(const std::vector<Side> & sides)
-{
-    const auto count{sides.size() - 1};
-    if (count <= 0)
-        return false;
-
-    // Calculate average length of all previous sides.
-    auto lambda = [](size_t a, const Side & b) { return a + b.getDuration(); };
-    size_t total = std::accumulate(sides.begin(), std::prev(sides.end()), 0, lambda);
-    size_t average{total / count};
-
-    size_t last{sides.back().getDuration()};
-    if (last < ((average * 95) / 100))
-         return true;
-
-    return false;
-}
-#endif
-
-struct Used
+class Used
 {
     const Track * tp{};
     bool used{};
+
+public:
+    void init(const Track & track) { tp = &track; }
+    void setUsed(bool state = true) { used = state; }
+    bool isUsed(void) const { return used; }
+    size_t getSeconds(void) const { return tp->getSeconds(); }
+    const Track * getTp(void) const { return tp; };
+
 };
 static std::vector<Side> addTracksToSides(const std::vector<Track> & tracks, size_t duration, size_t count)
 {
@@ -152,7 +56,7 @@ static std::vector<Side> addTracksToSides(const std::vector<Track> & tracks, siz
     auto it = tracks.begin();
     for (int i = 0; i < trackCount; ++i, ++it)
     {
-        tracker[i].tp = &(*it);
+        tracker[i].init(*it);
     }
 
     // std::cout << "Add tracks to sides\n";
@@ -170,11 +74,10 @@ static std::vector<Side> addTracksToSides(const std::vector<Track> & tracks, siz
     int direction{1};
     std::vector<Side>::iterator forth{sides.begin()};
     std::vector<Side>::reverse_iterator back{sides.rbegin()};
-    // std::vector<Track>::const_iterator track{tracks.begin()};
     int index{};
     for (int attempts = 0; attempts < count; ++attempts)
     {
-        auto tp{tracker[index].tp};
+        auto tp{tracker[index].getTp()};
         if (forward)
         {
             if ((*forth).getDuration() + tp->getSeconds() > duration)
@@ -275,63 +178,7 @@ int shuffleTracksAcrossSides(void)
     size_t length{total/optimum};
     if (showDebug)
         std::cout << "Minimum side length " << secondsToTimeString(length) << "\n";
-#if 0
-    // Home in on optimum side length.
-    size_t lim{15};
-    size_t minimum{length};
-    size_t maximum{duration};
-    while (minimum <= maximum)
-    {
-        size_t median{(minimum + maximum + 1) / 2};
-        if (showDebug)
-            std::cout << "\nSuggested length " << secondsToTimeString(median) << "\n";
 
-        sides.clear();
-        sides = addTracksToSides(tracks, median);
-
-        if (showDebug)
-        {
-            std::cout << "Suggested sides\n";
-            for (const auto & side : sides)
-                std::cout << side.getTitle() << " - " << side.size() << " tracks " << secondsToTimeString(side.getDuration()) << "\n";
-        }
-
-        if ((median == minimum) || (median == maximum))
-        {
-            break;
-        }
-        else
-        if (leftCondition(optimum, sides.size()))
-        {
-            minimum = median;
-            if (showDebug)
-            {
-                std::cout << "Minimum set to " << secondsToTimeString(minimum) << "\n";
-                std::cout << "Maximum is " << secondsToTimeString(maximum) << "\n";
-            }
-        }
-        else
-        if (rightCondition(sides))
-        {
-            maximum = median;
-            if (showDebug)
-            {
-                std::cout << "Minimum is " << secondsToTimeString(minimum) << "\n";
-                std::cout << "Maximum set to " << secondsToTimeString(maximum) << "\n";
-            }
-        }
-        else
-        {
-            break;
-        }
-
-        if (--lim == 0)
-        {
-            std::cerr << "Abort!!!\n";
-            break;
-        }
-    }
-#endif
     std::cout << "\nThe recommended sides are\n";
     for (const auto & side : sides)
         std::cout << side.toString() << "\n";
